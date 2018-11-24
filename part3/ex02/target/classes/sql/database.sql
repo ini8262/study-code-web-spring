83453084bc03d6447d34a3d3818486f957507c6a
ini8262 <ini8262@gmail.com>
2018-11-24 15:12:50 +0900

    Chapter 08 까지

--- part3/ex02/target/classes/sql/database.sql
+++ part3/ex02/target/classes/sql/database.sql
@@ -11,3 +11,12 @@ create table tbl_board (
 
 alter table tbl_board add constraint pk_board 
 primary key (bno);
+
+insert into TBL_BOARD (bno, title, content, writer) values (seq_board.nextval, '테스트 제목1', '테스트 내용1', 'user01');
+insert into TBL_BOARD (bno, title, content, writer) values (seq_board.nextval, '테스트 제목2', '테스트 내용2', 'user02');
+insert into TBL_BOARD (bno, title, content, writer) values (seq_board.nextval, '테스트 제목3', '테스트 내용3', 'user03');
+insert into TBL_BOARD (bno, title, content, writer) values (seq_board.nextval, '테스트 제목4', '테스트 내용4', 'user04');
+insert into TBL_BOARD (bno, title, content, writer) values (seq_board.nextval, '테스트 제목5', '테스트 내용5', 'user05');
+insert into TBL_BOARD (bno, title, content, writer) values (seq_board.nextval, '테스트 제목6', '테스트 내용6', 'user06');
+insert into TBL_BOARD (bno, title, content, writer) values (seq_board.nextval, '테스트 제목7', '테스트 내용7', 'user07');
+insert into TBL_BOARD (bno, title, content, writer) values (seq_board.nextval, '테스트 제목8', '테스트 내용8', 'user08');
